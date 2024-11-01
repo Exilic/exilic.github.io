41066075c53102c22e2cdd82b1e09673ceed9083
Tobias Engberg <tobias.engberg@exilic.se>
2024-11-01 15:29:02 +0100

    Update Card.jsx with present GitHub user name

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ export const Card = ({index, links}) => {
 
     return (
         <div id="ContentCard">
-            <a key={index} href={"https://exilic.github.io/" + links[0]}>
+            <a key={index} href={"https://tobiasengberg.github.io/" + links[0]}>
                 <img src={`/img/${links[1]}`} alt={links[2]}/>
                 <section>
                     <h3><span>Subject:</span> {links[3][0]}</h3>
